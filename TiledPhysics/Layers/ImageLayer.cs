7c37175daaddd1541b082a7018f87e1facbd7fc6
marsmaantje <caspersikkens@planet.nl>
2022-04-28 19:38:20 +0200

    Added repeat x support

--- TiledPhysics/Layers/ImageLayer.cs
+++ TiledPhysics/Layers/ImageLayer.cs
@@ -16,18 +16,20 @@ namespace Layers
         public ImageLayer(TiledMapParser.TiledLoader loader, TiledMapParser.ImageLayer obj, Scene parentScene, float paralaxX = 1, float paralaxY = 1) : base(parentScene, paralaxX, paralaxY)
         {
             //duplicate the image a couple times if it should be repeated
-            bool repeating = obj.GetBoolProperty("Repeating", false);
+            bool repeating = obj.repeatX;
+            SpriteBatch batch = new SpriteBatch();
+            int spriteCount = repeating ? 40 : 1;
 
-            for (int i = 0; i < (repeating ? 10 : 1); i++)
+            for (int i = 0; i < spriteCount; i++)
             {
                 Sprite image = new Sprite(Path.Combine(loader.FolderName, obj.Image.FileName), false, false);
-                image.x = obj.offsetX + (image.width * i);
+                image.x = obj.offsetX + (image.width * (i - Mathf.Ceiling(spriteCount/2)));
                 image.y = obj.offsetY;
                 image.alpha = obj.Opacity;
-
-                AddChild(image);
-            }   
-
+                batch.AddChild(image);
+            }
+            batch.Freeze();
+            AddChild(batch);
         }
     }
 }
